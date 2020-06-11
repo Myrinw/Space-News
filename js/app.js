ea91f119f4de19de0873c69baa04194d97cf75ff
myrin <gebruiker@MacBook-Pro-van-Gebruiker.local>
2020-06-11 14:18:58 +0200

    cool typewriter effect

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+const title = document.querySelector('.hero__title-text');
+
+const typewriter = new Typewriter(title, {
+    loop: false
+});
+
+typewriter.typeString('SPACE')
+.pauseFor(400)
+.typeString("-NEWS")
+.start();
+
 async function fetchData(){
     const response = await fetch('https://api.nasa.gov/insight_weather/?api_key=mqxrxvst1lt1TQEUglgJUwg2ikHjr6c6lOwtjadR&feedtype=json&ver=1.0');
     const data = await response.json();
